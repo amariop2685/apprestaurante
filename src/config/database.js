3429fe05fc8227d460a612adf60b05d0de15a87a
amariop2685 <acostap.marioa@gmail.com>
2024-02-24 12:23:17 -0300

    cambio en conexion BD

--- src/config/database.js
+++ src/config/database.js
@@ -8,13 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDugJlmqrrS5ZE7PKAfnS5268UNh0S21uY",
-  authDomain: "apprestaurante-249aa.firebaseapp.com",
-  projectId: "apprestaurante-249aa",
-  storageBucket: "apprestaurante-249aa.appspot.com",
-  messagingSenderId: "444015986371",
-  appId: "1:444015986371:web:6a8ccd1cd0e7477d160476",
-  measurementId: "G-81GHPS2004"
+  apiKey: "AIzaSyCsJ3e_2AWzgzhd167iq3v9TH2C7P_MyjU",
+  authDomain: "restaurant-8ec55.firebaseapp.com",
+  projectId: "restaurant-8ec55",
+  storageBucket: "restaurant-8ec55.appspot.com",
+  messagingSenderId: "232674012572",
+  appId: "1:232674012572:web:f433bb8a70572679276c59"
 };
 
 // Initialize Firebase
